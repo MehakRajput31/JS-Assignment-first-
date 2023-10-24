de49343696a901acea38c2f694fb4faf0e71e112
MehakRajput31 <142160267+MehakRajput31@users.noreply.github.com>
2023-10-24 11:31:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+//alert("hello world");//
+
+
+//var a = 10;
+//var b = 5;
+//var c = a+b;
+//console.log(c)
+
+//var x = 5;
+//var y = "ali";
+//var z = y+x;
+//alert(z)
+
+/*var f =20;
+var g ="2";
+var h = f*g;
+alert(h)*/
+
+console.log ("Mehak Rajput");
+console.log ("hello world");
+prompt("Hi,How are you");
+console.log( Math.random()*100);
+
+let   String = 'laurence';
+let  = " Srekis";
+let  value = undefined;
+let   = null;
+let myNum = 1000
+ 
+console.log ("string", typeof String)
+console.log ( "srekis" , typeof String)
+console.log("value", typeof undefined)
+console.log("value",typeof null)
+console.log("myNum",typeof NaN)
+
+
+let str1 = "Hello,";
+let str2 = " My name is Mehak Rajput,";
+let str3 = " I am 21 years old and I can code JavaScript ";
+let str4 = "/true";
+let result2 = str1 + str2 +str3 + str4
+console.log(result2)
+
+// Write some code to calculate the hypotenuse of a triangle using the Pythagorean 
+//theorem when given the values of the other two sides. The theorem specifies that the 
+//relation between the sides of a right-angled triangle is a2+ b2= c2
+let a ;
+let b ;
+let c ; 
+
+a = window.prompt("Enter  side A");
+a = Number (a);
+b = window.prompt("Enter side B");
+b = Number (b)
+c =  Math.sqrt( Math.pow ( a ,2) + Math.pow( b,2) );
+ console.log ("Side C", c);
+
+
+ let d = 3;
+ let e = 4;
+ let f = 5;
+ let g = d + e;
+ let h = 3 / 5;
+ let i = 5%4
+ console.log (g)
+ console.log (h)
+ console.log (i)
+
+
+  
